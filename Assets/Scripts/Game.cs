c93b84a598e532cadeaf8cc50ee10ef2665aecc2
web3ten0 <web3ten0@gmail.com>
2022-09-01 17:48:08 +0900

    Materialの色をランダムに変更するようにした

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -7,6 +7,13 @@ public class Game : MonoBehaviour
     void Start()
     {
         var prefab = Resources.Load<GameObject>("Prefabs/Block");
+        var materials = new[] {
+            Resources.Load<Material>("Materials/Block1"),
+            Resources.Load<Material>("Materials/Block2"),
+            Resources.Load<Material>("Materials/Block3"),
+            Resources.Load<Material>("Materials/Block4"),
+            Resources.Load<Material>("Materials/Block5"),
+        };
         int x, z, i, j;
         for (i = 0; i < 5; i++)
         {
@@ -15,9 +22,10 @@ public class Game : MonoBehaviour
             {
                 x = -6 + j;
                 Vector3 position = new Vector3(x, 1, z);
-                Instantiate(prefab, position, Quaternion.identity);
+                GameObject block = Instantiate(prefab, position, Quaternion.identity);
+                block.GetComponent<MeshRenderer>().material = materials[Random.Range(0, 5)];
             }
         }
     }
-
 }
+
